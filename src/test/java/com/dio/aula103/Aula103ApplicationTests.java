e06d87b72f0f1b22b4233bb1685b0ff9939a2eb4
janillejanielle <janille.janielle2@gmail.com>
2022-08-29 19:27:02 -0300

    Segundo commit

---
+++ src/test/java/com/dio/aula103/Aula103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.aula103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula103ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
